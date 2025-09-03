e65c4c4b3006a1322db6bb637ddc7bddc79d679b
riyacoding007 <riyacoding007@gmail.com>
2025-09-03 20:42:22 +0530

    feat: scroll trigger animation added to the content

--- scrollingCoffeeGallery/script/script.js
+++ scrollingCoffeeGallery/script/script.js
@@ -1,21 +1,45 @@
-
 gsap.registerPlugin(ScrollTrigger);
 
-// Animate all right-column sections
-gsap.utils.toArray(".right-column").forEach((col) => {
-  gsap.from([col.querySelector("h1"), col.querySelector("p")], {
-    opacity: 0,
-    y: 50, // slide upward
-    duration: 1,
-    stagger: 0.2, // h1 then p
+gsap.utils.toArray(".coffee-section").forEach((section, index) => {
+  const img = section.querySelector(".left-column img");
+  const title = section.querySelector(".right-column h1");
+  const para = section.querySelector(".right-column p");
+
+  const tl = gsap.timeline({
     scrollTrigger: {
-      trigger: col,
-      start: "top 80%",   // when top of section hits 80% viewport
-      toggleActions: "play reverse play reverse", 
-      // onEnter | onLeave | onEnterBack | onLeaveBack
-      markers: false // set to true for debugging
+      trigger: section,
+      start: "top 65%",        // tweak this if you want earlier/later trigger
+      end: "bottom 70%",  // up scroll trigger zone
+      toggleActions: "play reverse play reverse",
+      // markers: true,       // set true while fine-tuning, then false
+      // once: false
     }
   });
+
+  // image enters from left, slightly scaled
+  tl.from(img, {
+    opacity: 0,
+    y: -120,
+    scale: 0,
+    duration: 0.85,
+    ease: "power3.out"
+  });
+
+  // title + paragraph slide in from right with overlap
+  tl.from(title, {
+    opacity: 0,
+    y: 80,
+    duration: 0.6,
+    ease: "power3.out"
+  }, "-=0.45");
+
+  tl.from(para, {
+    opacity: 0,
+    y: 80,
+    duration: 0.6,
+    ease: "power3.out"
+  }, "-=0.45");
 });
+
 
 
